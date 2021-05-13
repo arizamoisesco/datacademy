4fe1640a74c525846aa26507369739e81e70f2bc
Moises Ariza <arizamoises@gmail.com>
2021-05-12 22:00:51 -0500

    Conditions are created for analyzing and using the previously created functions

--- PYTHON CARDIO/area_triangulos.py
+++ PYTHON CARDIO/area_triangulos.py
@@ -1,5 +1,5 @@
-base = float(input("Ingrese la base del triangulo: "))
-altura = float(input("Ingrese la altura del trinagulo: "))
+base = float(input("Ingrese la base del triangulo en cm: "))
+altura = float(input("Ingrese la altura del trinagulo en cm: "))
 
 def area(b: float, h: float) -> float:
     resultado_area = (b*h)/2 
@@ -23,5 +23,19 @@ def isoceles(lado1: float, lado2: float, lado3: float) -> bool:
     else:
         return False 
 
-resultado = area(base, altura)
-print(resultado)
+if __name__ == '__main__':
+    resultado = area(base, altura)
+    print(f"El area de su triángulo es: {resultado} cm")
+    opcion_tipo_triangulo = input("¿Desea averiguar que tipo de triangulo tiene S/N?").upper()
+    if(opcion_tipo_triangulo == "S"):
+        print("Ingrese a continuación los otros lados restantes\n")
+        lado1 = float(input("lado 1: "))
+        lado2 = float(input("Lado 2: "))
+        if(escaleno(base, lado1, lado2)):
+            print("Su triangulo es escaleno")
+        elif(equilatero(base, lado1, lado2)):
+            print("Su triangulo es equilatero")
+        elif(isoceles(base, lado1, lado2)):
+            print("Su triángulo es isoceles")
+    else: 
+        print("Gracias por usuarme")
