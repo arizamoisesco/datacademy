635fca2675a65b6376ac29f4e1eed0cbdcc53fc4
Moises Ariza <arizamoises@gmail.com>
2021-05-13 23:15:10 -0500

    The functions were refactored and modularized

--- PYTHON CARDIO/PIEDRA_PAPEL_TIJERA/piedra_papel_tijera.py
+++ PYTHON CARDIO/PIEDRA_PAPEL_TIJERA/piedra_papel_tijera.py
@@ -1,85 +1,54 @@
 import random
+import ascii_manos as manos
 
-piedra = """
-
-    _______
----'   ____)
-      (_____)
-      (_____)
-      (____)
----.__(___)
-"""
-
-papel = """
-    _______
----'   ____)____
-          ______)
-          _______)
-         _______)
----.__________)
-"""
-
-tijeras = """
-    _______
----'   ____)____
-          ______)
-       __________)
-      (____)
----.__(___)
-"""
-
-
-def juego(jugador: int, computadora: int) -> str:
-    intentos = 0
-    puntos_computadora = 0
-    puntos_usuario = 0
-    while intentos < 3:
-        opciones = f"Piedra {piedra}", f"Papel{papel}", f"Tijera {tijeras}"
-        print(
-            f"Tu escogiste {opciones[mano_usuario]} y el computador {opciones[mano_cpu]}"
-        )
-
-        if mano_usuario == 0 and mano_cpu == 0:
-            print("Quedaron empatados")
-        elif mano_usuario == 0 and mano_cpu == 1:
-            print("Perdiste punto para la computadora")
-            puntos_computadora += 1
-        elif mano_usuario == 0 and mano_cpu == 2:
-            print("Ganaste punto para ti")
-            puntos_usuario += 1
-        elif mano_usuario == 1 and mano_cpu == 0:
-            print("Ganaste punto para ti")
-            puntos_usuario + 1
-        elif mano_usuario == 1 and mano_cpu == 1:
-            print("Quedaron empatados")
-        elif mano_usuario == 1 and mano_cpu == 2:
-            print("Perdiste: tijera corta papel")
-            puntos_computadora += 1
-        elif mano_usuario == 2 and mano_cpu == 0:
-            print("Perdiste: La roca destruye la tijera")
-            puntos_computadora += 1
-        elif mano_usuario == 2 and mano_cpu == 1:
-            print("Ganaste")
-            puntos_usuario += 1
-        elif mano_usuario == 2 and mano_cpu == 2:
-            print("Quedaron empatados")
-        else:
-            print("Seleccionaste una opción incorrecta")
-
-        intentos += 1
-
+def definir_ganador(puntos_computadora: int, puntos_usuario: int)-> str:
     if puntos_usuario > puntos_computadora:
-        return "El ganador es el usuario"
+        return " 🥳El ganador es el usuario 😛"
     elif puntos_usuario < puntos_computadora:
-        return "La Computadora ha ganado"
+        return "🤕La Computadora ha ganado 💻"
     else:
-        return "Quedaron empatados"
+        return "Quedaron empatados🤝"
+
+
+def juego(jugador: int, computadora: int) -> int:
+    opciones = f"Piedra {manos.piedra}", f"Papel{manos.papel}", f"Tijera {manos.tijera}"
+    print(f"Tu escogiste {opciones[mano_usuario]} y el computador {opciones[mano_cpu]}")
+
+    if(mano_usuario == 0 and mano_cpu == 0) or (mano_usuario == 1 and mano_cpu == 1) or (mano_usuario == 2 and mano_cpu == 2):
+        return 0 #Empatados
+    elif (mano_usuario == 0 and mano_cpu == 1) or (mano_usuario == 1 and mano_cpu == 2) or (mano_usuario == 2 and mano_cpu == 0):
+        return 1 #Gano un punto la computadora
+    elif (mano_usuario == 0 and mano_cpu == 2) or (mano_usuario == 1 and mano_cpu == 0) or (mano_usuario == 2 and mano_cpu == 1):
+        return 2 #Ganaste un punto
+    else:
+        print("Seleccionaste una opción incorrecta")
 
 
 if __name__ == "__main__":
     print("Bienvenido al juego de piedra, papel y tijera")
-    mano_usuario = int(
-        input("¿Cuál opción deseas? Escribe piedra(0), papel(1), tijeras(2)")
-    )
-    mano_cpu = random.randint(0, 2)
-    juego(mano_usuario, mano_cpu)
+
+    punto_computadora = 0
+    punto_usuario = 0
+    numero_ronda = 1
+    while numero_ronda < 4:
+        print("*"*20)
+        mano_usuario = int(input("¿Cuál opción deseas? Escribe piedra(0), papel(1), tijeras(2): "))
+        mano_cpu = random.randint(0, 2)
+
+        punto = juego(mano_usuario, mano_cpu)
+        if punto == 1: 
+            punto_computadora += 1
+            print(f"Ronda {numero_ronda} gana computadora")
+        elif punto == 2:
+            punto_usuario += 1
+            print(f"Ronda {numero_ronda} ganaste")
+        else:
+            print(f"Ronda {numero_ronda} quedaron empatados")
+
+        numero_ronda += 1
+
+    print(definir_ganador(punto_computadora, punto_usuario))
+    
+    
+
+
