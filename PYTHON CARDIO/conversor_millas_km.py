d7cc8c1eb1dd21dc8aeda62845fd56b4ace55619
Moises Ariza <arizamoises@gmail.com>
2021-05-17 18:08:52 -0500

    Reformatted to follow PEP 8

--- PYTHON CARDIO/conversor_millas_km.py
+++ PYTHON CARDIO/conversor_millas_km.py
@@ -1,18 +1,18 @@
-#Recibimos los datos del usuario convertidos a números
-
 def conversion(opcion: str) -> str:
-    if(opcion == "a"):
+    if opcion == "a":
         distancia = float(input("Ingrese la distancia en km que desea convertir: "))
-        kilometros = distancia / 1.609344 
-        return f"El resultado de su conversión es: {kilometros} millas"
-    elif(opcion == "b"):
+        millas = distancia / 1.609344
+        return f"El resultado de su conversión es: {millas} millas"
+    elif opcion == "b":
         distancia = float(input("Ingrese la distancia en millas que desea convertir: "))
-        kilometros = distancia * 1.609344 
+        kilometros = distancia * 1.609344
         return f"El resultado de su conversión es: {kilometros} km"
+
 
 if __name__ == "__main__":
     print("Bienvenido señor usuario")
-    operacion_deseada = input("¿Qué conversión desea realizar?\n(a)km -> millas\n(b)millas -> km\nIngrese la letra de la opción deseada: ").lower()
+    operacion_deseada = input(
+        "¿Qué conversión desea realizar?\n(a)km -> millas\n(b)millas -> km\nIngrese la letra de la opción deseada: "
+    ).lower()
     respuesta = conversion(operacion_deseada)
     print(respuesta)
-
