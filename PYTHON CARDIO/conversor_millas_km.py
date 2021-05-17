6ecdd7cc724088e50539fcabd54450d0955d0ba6
Moises Ariza <arizamoises@gmail.com>
2021-05-17 17:44:40 -0500

    Added functionalities for two types of conversions

--- PYTHON CARDIO/conversor_millas_km.py
+++ PYTHON CARDIO/conversor_millas_km.py
@@ -1,7 +1,15 @@
 #Recibimos los datos del usuario convertidos a números
 print("Bienvenido señor usuario")
-distancia = float(input("Ingrese la distancia que desea convertir: "))
+operacion_deseada = input("¿Qué conversión desea realizar?\n(a)km -> millas\n(b)millas -> km\nIngrese la letra de la opción deseada: ").lower()
+if(operacion_deseada == "a"):
+    distancia = float(input("Ingrese la distancia en km que desea convertir: "))
+    kilometros = distancia / 1.609344 
+    print(f"El resultado de su conversión es: {kilometros} millas")
+elif(operacion_deseada == "b"):
+    distancia = float(input("Ingrese la distancia en millas que desea convertir: "))
+    kilometros = distancia * 1.609344 
+    print(f"El resultado de su conversión es: {kilometros} km")
 #Ejecuto la conversión
-kilometros = distancia * 1.609344 
+
 #Genero el resultado en pantalla
-print(f"El resultado de su conversión es: {kilometros} km")
+
