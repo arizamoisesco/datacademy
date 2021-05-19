cbb2e724a15ca43c279da72afd5efa981f0acc2e
Moises Ariza <arizamoises@gmail.com>
2021-05-19 09:16:04 -0500

    Added function to compare numbers and know who is bigger and who is lower

--- PYTHON CARDIO/rangos_cambiantes.py
+++ PYTHON CARDIO/rangos_cambiantes.py
@@ -1,10 +1,22 @@
+def organizar_numeros(num1: int, num2: int) -> tuple:
+    if num1 > num2: 
+       numero_mayor, numero_menor = num1, num2
+    else:
+        numero_mayor, numero_menor = num2, num1
+
+    return numero_mayor, numero_menor
 
 if __name__ == "__main__":
-    limite_superior = int(input("Ingrese un número: "))
-    limite_inferior = int(input("Ingrese otro número: "))
+    num1 = int(input("Ingrese un número: "))
+    num2 = int(input("Ingrese otro número: "))
     comparacion = int(input("Ingrese el número a comparar: "))
 
+    limite_superior, limite_inferior = organizar_numeros(num1, num2)
+    print("*"*30)
+    print(f"Rango superior: {limite_superior}\nRango inferior: {limite_inferior}\nNúmero a comparar: {comparacion}")
+    print("*"*30)
     if comparacion > limite_inferior and comparacion < limite_superior:
         print(f"El número {comparacion} esta dentro del rango")
     else:
         print(f"El número {comparacion} esta fuera del rango")
+    print("*"*30)
